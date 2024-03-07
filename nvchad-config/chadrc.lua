5b6fbe80135354b150219c7d60edb51a2b58a82a
dannc <dannc.sao@gmail.com>
2024-03-07 16:00:08 +0700

    fix

--- nvchad-config/chadrc.lua
+++ nvchad-config/chadrc.lua
@@ -6,7 +6,7 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "one_light",
-  theme_toggle = { "one_light", "onedark" },
+  theme_toggle = { "one_light", "doomchad" },
   transparency = true,
 
   hl_override = highlights.override,
