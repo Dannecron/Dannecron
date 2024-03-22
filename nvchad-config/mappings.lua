abc2ca63257631a8fbb7e9b0d04d5cef2c4eceef
dannc <dannc.sao@gmail.com>
2024-03-22 10:35:05 +0700

    remove some keybindings

--- nvchad-config/mappings.lua
+++ nvchad-config/mappings.lua
@@ -8,32 +8,14 @@ M.general = {
         require("nvterm.terminal").toggle "vertical"
       end,
     },
-    ["<C-\\>"] = {
-      function()
-        require("nvterm.terminal").toggle "horizontal"
-      end,
-    },
-    ["\\"] = { "<cmd>:vsplit <CR>", "Vertical split" },
-    --  format with conform
-    ["<leader>fm"] = {
-      function()
-        require("conform").format()
-      end,
-      "formatting",
-    }
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
   },
   t = {
     ["<C-]>"] = {
       function()
         require("nvterm.terminal").toggle "vertical"
-      end,
-    },
-    ["<C-\\>"] = {
-      function()
-        require("nvterm.terminal").toggle "horizontal"
       end,
     },
   },
